2464034db1102798f5f8dcea9218032566dddb8b
fikpat66 <fikrinursyahbani@gmail.com>
2024-09-21 16:48:20 +0700

    update mart training

--- data_warehouse/models/mart/training.sql
+++ data_warehouse/models/mart/training.sql
@@ -9,6 +9,7 @@ SELECT
     t."TrainingID",
     p."EmployeeID",
     t."TrainingProgram",
-    (p."FinishTraining" - p."StartTraining") AS TrainingDuration
+    (p."FinishTraining" - p."StartTraining") AS TrainingDuration,
+    p."StatusTraining"
 FROM {{ source('public', 'payroll') }} AS p
 LEFT JOIN {{ source('public', 'training') }} AS t ON t."TrainingID" = p."TrainingID"
